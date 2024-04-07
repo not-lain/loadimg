90cc5c983e96b961ba835cf572e96bfe23014491
Lain <hhichri60@gmail.com>
2024-04-07 14:26:13 +0100

    format code with ruff

--- src/loadimg/utils.py
+++ src/loadimg/utils.py
@@ -19,8 +19,8 @@ def download_image(url: str):
             if "/view" in url or url.endswith("/"):
                 url = "/".join(url.split("/")[:-1])
             url = "https://drive.google.com/uc?id=" + url.split("/")[-1]
-        elif "hf.co" or "huggingface.co" in url : 
-            url = url.replace("/blob/","/resolve")
+        elif "hf.co" or "huggingface.co" in url:
+            url = url.replace("/blob/", "/resolve")
         response = requests.get(url, timeout=5)  # Timeout set to 5 seconds
         response.raise_for_status()  # Raise an exception for HTTP errors
         return Image.open(BytesIO(response.content))
