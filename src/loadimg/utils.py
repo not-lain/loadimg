8ff908582969809a987f9faec1f38cff92f64367
Lain <hhichri60@gmail.com>
2024-08-30 20:00:44 +0100

    switch to a better typing method and improve error messages.

--- src/loadimg/utils.py
+++ src/loadimg/utils.py
@@ -6,32 +6,10 @@ from PIL import Image
 import numpy as np
 import tempfile
 import base64
+import inspect
+
 # TODO:
-# support output_type parameter to change the output type
-# support other input types such as lists, Bytes, tensors, torch tensors, ...
-
-
-def download_image(url: str):
-    """A function to get a Pillow image from a URL."""
-    try:
-        if "github" in url and "raw=true" not in url:
-            url += "?raw=true"
-        elif "drive" in url and "uc?id=" not in url:
-            if "/view" in url or url.endswith("/"):
-                url = "/".join(url.split("/")[:-1])
-            url = "https://drive.google.com/uc?id=" + url.split("/")[-1]
-        elif "hf.co" or "huggingface.co" in url:
-            url = url.replace("/blob/", "/resolve")
-        response = requests.get(url, timeout=5)  # Timeout set to 5 seconds
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        return Image.open(BytesIO(response.content))
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading image from {url}: {e}")
-        return None  # Return None if there's an error
-
-
-SUPPORTED_INPUT_TYPES = Union[str, bytes, np.ndarray, Image.Image]
-SUPPORTED_OUTPUT_TYPES = Literal["pil", "numpy", "str", "base64"]
+# support other input types such as lists, tensors, ...
 
 
 def load_img(
@@ -67,7 +45,26 @@ def load_img(
         return img_b64
 
 
-def load(img: SUPPORTED_INPUT_TYPES) -> Image.Image:
+def download_image(url: str):
+    """A function to get a Pillow image from a URL."""
+    try:
+        if "github" in url and "raw=true" not in url:
+            url += "?raw=true"
+        elif "drive" in url and "uc?id=" not in url:
+            if "/view" in url or url.endswith("/"):
+                url = "/".join(url.split("/")[:-1])
+            url = "https://drive.google.com/uc?id=" + url.split("/")[-1]
+        elif "hf.co" or "huggingface.co" in url:
+            url = url.replace("/blob/", "/resolve")
+        response = requests.get(url, timeout=5)  # Timeout set to 5 seconds
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        return Image.open(BytesIO(response.content))
+    except requests.exceptions.RequestException as e:
+        print(f"Error downloading image from {url}: {e}")
+        return None  # Return None if there's an error
+
+
+def load(img) -> Image.Image:
     """loads the img and returns a pillow image"""
     # base64 (str or bytes)
     if isBase64(img):
@@ -90,17 +87,27 @@ def load(img: SUPPORTED_INPUT_TYPES) -> Image.Image:
     # pillow image
     elif isinstance(img, Image.Image):
         return img
+    else:
+        available_types = [
+            i.__name__
+            for i in inspect.signature(load_img).parameters["img"].annotation.__args__
+        ]
+        available_types = ", ".join(available_types)
+        raise ValueError(
+            f"expected one of the following types :{available_types}, but got {type(img).__name__}"
+        )
+
 
 def isBase64(sb):
-        """
-        checks if the input object is base64
-        """
-        try:
-            if isinstance(sb, str):
-                    # If there's any unicode here, an exception will be thrown and the function will return false
-                    sb_bytes = bytes(sb, 'ascii')
-            elif isinstance(sb, bytes):
-                    sb_bytes = sb
-            return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
-        except Exception:
-            return False
+    """
+    checks if the input object is base64
+    """
+    try:
+        if isinstance(sb, str):
+            # If there's any unicode here, an exception will be thrown and the function will return false
+            sb_bytes = bytes(sb, "ascii")
+        elif isinstance(sb, bytes):
+            sb_bytes = sb
+        return base64.b64encode(base64.b64decode(sb_bytes)) == sb_bytes
+    except Exception:
+        return False
